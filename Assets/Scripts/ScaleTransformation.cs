57def9c7c1ec686511c849ba88b31f873cae980e
versatus <86527888+seacomit@users.noreply.github.com>
2023-06-30 11:05:42 -0400

    add scale transformation

---
+++ Assets/Scripts/ScaleTransformation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleTransformation : Transformation
+{
+    [SerializeField]
+    Vector3 scale;
+
+    public override Vector3 Apply(Vector3 point)
+    {
+        point.x *= scale.x;
+        point.y *= scale.y;
+        point.z *= scale.z;
+        return point;
+    }
+}
