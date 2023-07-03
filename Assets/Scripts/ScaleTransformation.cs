e61f1bb0b00d9c8ad1029ff96381fe94246398e5
versatus <86527888+seacomit@users.noreply.github.com>
2023-07-03 16:32:09 -0400

    Convert all transformations to use Matrix4x4 transforms. So cool!

--- Assets/Scripts/ScaleTransformation.cs
+++ Assets/Scripts/ScaleTransformation.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ScaleTransformation : Transformation
 {
     [SerializeField]
     Vector3 scale;
 
-    public override Vector3 Apply(Vector3 point)
+    public override Matrix4x4 Matrix
     {
-        point.x *= scale.x;
-        point.y *= scale.y;
-        point.z *= scale.z;
-        return point;
+        get
+        {
+            Matrix4x4 matrix = new Matrix4x4();
+            matrix.SetRow(0, new Vector4(scale.x, 0f, 0f, 0f));
+            matrix.SetRow(1, new Vector4(0f, scale.y, 0f, 0f));
+            matrix.SetRow(2, new Vector4(0f, 0f, scale.z, 0f));
+            matrix.SetRow(3, new Vector4(0f, 0f, 0f,      1f));
+
+            return matrix;
+        }
     }
 }
