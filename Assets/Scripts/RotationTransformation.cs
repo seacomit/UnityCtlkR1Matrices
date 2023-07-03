774f97aaddbba72c431faf1274469634ecfdcfb4
versatus <86527888+seacomit@users.noreply.github.com>
2023-07-03 11:30:22 -0400

    apply rotation about all axis

--- Assets/Scripts/RotationTransformation.cs
+++ Assets/Scripts/RotationTransformation.cs
@@ -9,14 +9,32 @@ public class RotationTransformation : Transformation
 
     public override Vector3 Apply(Vector3 point) 
     {
+        float radX = rotation.x * Mathf.Deg2Rad;
+        float radY = rotation.y * Mathf.Deg2Rad;
         float radZ = rotation.z * Mathf.Deg2Rad;
+        float sinX = Mathf.Sin(radX);
+        float cosX = Mathf.Cos(radX);
+        float sinY = Mathf.Sin(radY);
+        float cosY = Mathf.Cos(radY);
         float sinZ = Mathf.Sin(radZ);
         float cosZ = Mathf.Cos(radZ);
 
-        return new Vector3(
-            point.x * cosZ - point.y * sinZ,
-            point.x * sinZ + point.y * cosZ,
-            point.z
+        Vector3 xAxis = new Vector3(
+            cosY * cosZ,
+            cosX * sinZ + cosZ * sinX * sinY,
+            sinX * sinZ - cosX * cosZ * sinY
         );
+        Vector3 yAxis = new Vector3(
+            -cosY * sinZ,
+            cosX * cosZ - sinX * sinY * sinZ,
+            cosZ * sinX + cosX * sinY * sinZ
+        );
+
+        Vector3 zAxis = new Vector3(
+            sinY,
+            -cosY * sinX,
+            cosX * cosY
+        );
+        return xAxis * point.x + yAxis * point.y + zAxis * point.z;
     }
 }
