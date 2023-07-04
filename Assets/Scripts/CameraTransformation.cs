eec05e949adb23f747735b106b6495a4ba2fef91
versatus <86527888+seacomit@users.noreply.github.com>
2023-07-04 11:47:23 -0400

    Add rudimentary perspective projection

--- Assets/Scripts/CameraTransformation.cs
+++ Assets/Scripts/CameraTransformation.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraTransformation : Transformation
 {
+    [SerializeField]
+    float focalLength = 1f;
+
     public override Matrix4x4 Matrix
     {
         get
         {
             Matrix4x4 matrix = new Matrix4x4();
-            matrix.SetRow(0, new Vector4(1f, 0f, 0f, 0f));
-            matrix.SetRow(1, new Vector4(0f, 1f, 0f, 0f));
+            matrix.SetRow(0, new Vector4(focalLength, 0f, 0f, 0f));
+            matrix.SetRow(1, new Vector4(0f, focalLength, 0f, 0f));
             matrix.SetRow(2, new Vector4(0f, 0f, 0f, 0f));
-            matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+            matrix.SetRow(3, new Vector4(0f, 0f, 1f, 0f));
 
             return matrix;
         }
